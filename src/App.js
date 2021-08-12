dd5e5e822c8af62c48288253f216022ee4635fd2
rdperry031 <rdperry031@gmail.com>
2021-08-12 16:06:20 -0500

    completed project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
-import axios from 'axios'
 import Photo from './components/photo'
-
+import axios from 'axios'
 function App() {
- const [photoOfTheDay, setPhotOfTheDay] =useState('')
+  const [photoOfTheDay, setPhotoOfTheDay] = useState('')  
+
   useEffect(() => {
-   axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-    .then(res => {
-      console.log(res.data)
-      setPhotOfTheDay(res.data)
-    })
-    .catch(err => {
-      console.log(err)
-    })
- },[])
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+      .then(res => {
+        setPhotoOfTheDay(res.data)
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },[])
+  
+  
+  
+
   return (
     <div className="App">
-      <Photo photo = {photoOfTheDay} />
+    <Photo photo = {photoOfTheDay}/>
     </div>
   );
 }
 
 export default App;
+
+
+// Read through the instructions in the README.md file to build your NASA
+// app! Have fun <span role="img" aria-label='go!'>🚀</span>!
